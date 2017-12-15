bc12d9bd9899a6bb9f425a7ea81b854b1e98784a
Kiran Hegde <kiran.hegde121@gmail.com>
2017-12-15 07:32:56 +0530

    Initial commit, main.py modified as in walkthrough

--- main.py
+++ main.py
@@ -32,8 +32,18 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
+
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+
+    input_images = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    vgg_layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    vgg_layer4_out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    vgg_layer7_out = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
     
-    return None, None, None, None, None
+    return input_images, keep_prob, vgg_layer3_out, vgg_layer4_out, vgg_layer7_out
+
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,7 +57,19 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    return None
+    conv_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, [1, 1], padding = 'same',
+                                kernel_regularizer = tf.contrib.layers.l2_regularizer(1e-3))
+
+    deconv_0 = tf.layers.conv2d_transpose(conv_1x1, num_classes, 4, [2, 2], padding = 'same',
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    layer8_out = tf.add(deconv_0, vgg_layer3_out)
+
+    deconv_1 = tf.layers.conv2d_transpose(layer8_out, num_classes, 16, [8, 8], padding = 'same',
+                                          kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    layer9_out = tf.add(deconv_1, vgg_layer4_out)
+
+    return layer9_out
+
 tests.test_layers(layers)
 
 
@@ -61,7 +83,15 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    labels = tf.reshape(correct_label, (-1, num_classes))
+    cross_entropy_loss = tf.nn.softmax_cross_entropy_with_logits(logits, labels)
+    loss_operation = tf.reduce_mean(cross_entropy_loss)
+    optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
+    train_op = optimizer.minimize(loss_operation)
+
+    return (logits, train_op, cross_entropy_loss)
+
 tests.test_optimize(optimize)
 
 
@@ -81,11 +111,21 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
+    for epoch in range(epochs):
+        for images, labels in get_batches_fn(batch_size):
+            # Training
+            sess.run(train_op, feed_dict={input_image:images, correct_label:labels,
+                                          keep_prob:1, learning_rate:0.01})
+            print(sess.run(cross_entropy_loss))
     pass
+
 tests.test_train_nn(train_nn)
 
 
 def run():
+    epochs = 200
+    batch_size = 10
+
     num_classes = 2
     image_shape = (160, 576)
     data_dir = './data'
@@ -109,9 +149,12 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
+        input_image, keep_prob, vgg_layer3_out, vgg_layer4_out, vgg_layer7_out = load_vgg(sess, vgg_path)
+        output_layer = layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes)
 
         # TODO: Train NN using the train_nn function
-
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
+                 correct_label, keep_prob, learning_rate)
         # TODO: Save inference data using helper.save_inference_samples
         #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
